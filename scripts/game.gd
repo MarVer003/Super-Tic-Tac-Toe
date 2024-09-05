8b406a6f1da9b6bd6557d40e3f2e234c6ad9db19
MarVer003 <martin.versnjak@gmail.com>
2024-09-05 22:01:25 +0200

    Connecting backend with frontend

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,43 +7,33 @@ const Turn = preload("res://scripts/turn.gd").Turn
 var player_turn := Turn.X
 
 var grid_focus : GridContainer = null
-var subgames : Dictionary
+var subgames : Array[GridContainer]
 var playable_subgames : Array[GridContainer]
 var finished_subgames : Array[GridContainer]
+var super_tic_tac_toe : SuperTicTacToe
 
 func _ready() -> void:
 	node_main_grid.initiate_board()
+	super_tic_tac_toe = SuperTicTacToe.new()
 
 
-func _on_main_grid_sig_button_pressed(btn: Button) -> void:
-	var player_text = "X"
-	if player_turn == Turn.O:
-		player_text = "O"
-	
-	btn.text = player_text
-	btn.disabled = true
-	
-	var tic_tac_toe_subgame : TicTacToe_deprecated = btn.get_parent().get_child(0)
-	var position := int(str(btn.name).split("btn")[1])
-	
-	tic_tac_toe_subgame.draw_move(position, player_turn)
-	print(tic_tac_toe_subgame.board)
-	
-	if subgames[btn.name.split("btn")[1]] in playable_subgames:
-		grid_focus = subgames[btn.name.split("btn")[1]]
-		enable_grid(subgames[btn.name.split("btn")[1]])
-	
-	for sg in playable_subgames:
-		if sg != grid_focus:
-			disable_grid(sg)
-	
-	if tic_tac_toe_subgame.who_won != null:
-		finished_subgames.append(playable_subgames.pop_at(playable_subgames.find(btn.get_parent())))
-		for obj in btn.get_parent().get_children():
-			if(obj.name.contains("btn")):
-				obj.disabled = true
+func _on_main_grid_sig_button_pressed(btn: Button, grid : GridContainer) -> void:
+	var subgame_index := int(grid.name)
+	var cell_index := int(btn.name)
+	btn.text = super_tic_tac_toe.move(subgame_index, cell_index)
 
-	player_turn = Turn.O if player_turn == Turn.X  else Turn.X
+	if subgames[cell_index] in playable_subgames:
+		grid_focus = subgames[cell_index]
+		enable_grid(subgames[cell_index])
+	
+	for subgame in playable_subgames:
+		if subgame != grid_focus:
+			disable_grid(subgame)
+	
+	enable_grid(subgames[super_tic_tac_toe.focused_subboard.id])
+	
+	# Make the game playable
+
 
 func disable_grid(subgame : GridContainer):
 	for obj in subgame.get_children():
@@ -58,4 +48,4 @@ func enable_grid(subgame : GridContainer):
 
 func _on_main_grid_sig_grid_added(grid: GridContainer) -> void:
 	playable_subgames.append(grid)
-	subgames[grid.name.split("grid")[1]] = grid
+	subgames.append(grid)
--- scripts/main_grid.gd
+++ scripts/main_grid.gd
@@ -1,6 +1,6 @@
 extends GridContainer
 
-signal sig_button_pressed(button : Button)
+signal sig_button_pressed(button : Button, grid : GridContainer)
 signal sig_grid_added(grid : GridContainer)
 
 var width := 190
@@ -8,22 +8,21 @@ var height := width
 
 
 func initiate_board() -> void:
-	# For grids inside the main grid ##
+	# For grids inside the main grid #
 	for i in range(9):
 		var grid = GridContainer.new()
 		grid.custom_minimum_size = Vector2(width, height)
 		grid.columns = 3
-		grid.add_child(TicTacToe_deprecated.new(i))
-		grid.name = "grid" + str(i)
+		grid.name = i
 		
-		# For buttons (cells) inside every smaller grid ##
+		# For buttons (cells) inside every smaller grid #
 		for j in range(9):
 			var btn = Button.new()
 			btn.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 			btn.size_flags_vertical = Control.SIZE_EXPAND_FILL
-			btn.name = "btn" + str(j)
+			btn.name = j
 			btn.add_theme_stylebox_override("focus", StyleBox.new())
-			btn.connect("pressed", _on_button_pressed.bind(btn))
+			btn.connect("pressed", _on_button_pressed.bind(btn, btn.get_parent()))
 			grid.add_child(btn)
 		
 		add_child(grid)
@@ -31,6 +30,6 @@ func initiate_board() -> void:
 		
 
 
-func _on_button_pressed(btn: Button) -> void:
-	sig_button_pressed.emit(btn)
+func _on_button_pressed(btn: Button, grid : GridContainer) -> void:
+	sig_button_pressed.emit(btn, grid)
 	
